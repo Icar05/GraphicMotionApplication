2c561cc68b39a7c3531dcee6007ead38c0c38a1a
iPinguin <galiatkin.o@gmail.com>
2019-02-08 18:36:22 +0200

    first commit

---
+++ GraphicMotionApplication/custom/UIGraphicView.swift
@@ -0,0 +1,21 @@
+//
+//  UIGraphicView.swift
+//  GraphicMotionApplication
+//
+//  Created by Eddson on 08.02.2019.
+//  Copyright © 2019 ipinguin_linuxoid. All rights reserved.
+//
+
+import UIKit
+
+class UIGraphicView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
