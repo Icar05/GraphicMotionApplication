b37a2cd9e1f933b1e1678d89ca95b43aba8a8bef
iPinguin <galiatkin.o@gmail.com>
2019-02-08 12:05:01 +0200

    Initial Commit

---
+++ GraphicMotionApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphicMotionApplication
+//
+//  Created by Eddson on 08.02.2019.
+//  Copyright © 2019 ipinguin_linuxoid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
