601f722c68bb07d7c0587c50533e265d14c2a1f2
Icar05 <galiatkin.o@gmail.com>
2019-02-09 02:53:26 +0200

    fix disposable error

--- GraphicMotionApplication/ViewController.swift
+++ GraphicMotionApplication/ViewController.swift
@@ -12,13 +12,25 @@ import RxSwift
 class ViewController: UIViewController {
 
     
+    
+    var bag = DisposeBag()
+    
     @IBAction func huy(_ sender: Any) {
         testGraphic()
-    
     }
+    
+    
+    
     @IBOutlet weak var button: UIGraphicView!
-    let values :[Int] = [10, 5, 1, 5, 2, 5, 2, 5, 6, 3, 2, 5, 7, 2, 7, 3, 7, 7, 2, 7, 2, 7 , 1, 3, 5, 2, 4
-    , 4, 6, 7, 2, 3, 6, 7, 3, 7, 7]
+    
+    
+    
+    let values :[Int] = [10, 5, 1, 5, 2, 5,
+                         2, 5, 6, 3, 2, 5,
+                         7, 2, 7, 3, 7, 7,
+                         2, 7, 2, 7 , 1, 3,
+                         5, 2, 4, 4, 6, 7,
+                         2, 3, 6, 7, 3, 7, 7]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,20 +47,17 @@ class ViewController: UIViewController {
     
     
     func testGraphic(){
-        
 
-        Observable.zip(
+        (Observable.zip(
             Observable.from(values),
             Observable<Int>.interval(RxTimeInterval(0.1),
             scheduler: MainScheduler.instance))
             .observeOn(MainScheduler.instance)
             .subscribe(onNext: { (value, key) in
-                
-                
                 self.button.addStep(value: value)
                 print("value -> \(value)")
-//                self.graphic.setNeedsDisplay()
-            })
+            }) as Disposable)
+             .disposed(by: bag)
         
     }
 
